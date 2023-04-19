88f46388764326e3c18d38a9c4412a512d5ef1e6
alyx <alexanderbek3@gmail.com>
2023-04-19 11:43:01 +0930

    generalised, added headers abatraction

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 #![allow(nonstandard_style)]
 use dotenv::dotenv;
-use reqwest::{Client, Error};
+use reqwest::{
+    header::{self, HeaderMap},
+    Client, Error,
+};
 use serde_json;
+
+static USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv().ok();
 
-    let ALIENVAULT_API_TOKEN = std::env::var("ALIENVAULT_APIKEY").expect("No token found");
-
     // TODO turn this into potentially a query builder
     let request_url =
         "https://otx.alienvault.com/api/v1/pulses/user/AlienVault?2023-01-01T12:35:00+00:00";
 
-    let response = Client::new()
-        .get(request_url)
-        .header("X-OTX-API-KEY", ALIENVAULT_API_TOKEN)
-        .send()
-        .await?;
+    let client = Client::builder()
+        .default_headers(build_header())
+        .user_agent(USER_AGENT)
+        .build()?;
+
+    let response = client.get(request_url).send().await?;
 
     let status = response.status().clone();
     let headers = response.headers().clone();
@@ -31,4 +35,12 @@ async fn main() -> Result<(), Error> {
     println!("Serialized: {:#?}", r);
 
     Ok(())
+}
+
+/// Builds default headers for the client based on the API token
+fn build_header() -> HeaderMap {
+    let mut headers = header::HeaderMap::new();
+    let ALIENVAULT_API_TOKEN = std::env::var("ALIENVAULT_APIKEY").expect("No token found");
+    headers.insert("X-OTX-API-KEY", ALIENVAULT_API_TOKEN.parse().unwrap());
+    headers
 }
